80261460ebaea92045b3c8630bdd3825b097c6d5
christian bianchini <max246@gmail.com>
2026-02-11 18:21:00 +0000

    fixed battery sensor

--- custom_components/pettracer/sensor.py
+++ custom_components/pettracer/sensor.py
@@ -203,7 +203,7 @@ class PetTracerBatteryVoltageSensor(PetTracerBaseSensor):
         self._attr_unique_id = f"{device_id}_battery_voltage"
         self._attr_name = "Battery Voltage"
         self._attr_native_unit_of_measurement = "mV"
-        self._attr_device_class = SensorDeviceClass.BATTERY
+        self._attr_device_class = SensorDeviceClass.VOLTAGE
         self._attr_state_class = SensorStateClass.MEASUREMENT
         self._attr_icon = "mdi:battery"
 
